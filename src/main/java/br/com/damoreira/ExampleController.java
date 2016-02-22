2ba4fec86ddcb1bce876f788d5b5138a63979fc5
Guilherme D'Amoreira <guilherme@damoreira.com.br>
2016-02-22 18:09:34 -0300

    ajustes

--- src/main/java/br/com/damoreira/ExampleController.java
+++ src/main/java/br/com/damoreira/ExampleController.java
@@ -5,15 +5,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @RestController
 public class ExampleController {
 
 	@RequestMapping(value = "/{barcode}", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public void validate(@PathVariable String barcode) {
+	@ResponseStatus(HttpStatus.OK)
+	public void validate(@PathVariable String barcode) throws Exception {
 		System.out.println(String.format("Barcode: %s", barcode));
+		if ("200".equals(barcode)) {
+			return;
+		}
+		throw new Exception("Not 200");
 	}
+	
+	@ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public String handleIOException(Throwable ex) {
+        return ex.toString();
+    }
 
 }
